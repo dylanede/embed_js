d2224961944d7d492705b89cc8316865944d6e40
Dylan Ede <dylanede@googlemail.com>
2017-12-21 21:49:53 +0000

    Updated simple example to demonstrate code generation support.

--- examples/simple/subcrate/src/lib.rs
+++ examples/simple/subcrate/src/lib.rs
@@ -3,7 +3,14 @@ extern crate embed_js;
 
 embed_js_preamble!();
 
+// demonstration of using the js macro inside generated source - see the build script
+mod generated {
+    include!(concat!(env!("OUT_DIR"), "/generated_source.rs"));
+}
+
+
 pub fn add_one(x: i32) -> i32 {
+    generated::foo();
     js!([x as i32] -> i32 {
         return x + 1;
     })
